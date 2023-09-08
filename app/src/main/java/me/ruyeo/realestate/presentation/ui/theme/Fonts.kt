8615aa2d1e0b56d71904adac110f20d3c4278eaa
Sarvar Masharipov <63916119+sarvarmasharipov72@users.noreply.github.com>
2023-09-08 22:28:58 +0500

    add font inter

---
+++ app/src/main/java/me/ruyeo/realestate/presentation/ui/theme/Fonts.kt
@@ -0,0 +1,12 @@
+package me.ruyeo.realestate.presentation.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import me.ruyeo.realestate.R
+
+val inter = FontFamily(
+    Font(R.font.inter_light),
+    Font(R.font.inter_regular),
+    Font(R.font.inter_medium),
+    Font(R.font.inter_bold),
+)
